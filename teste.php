5481b3a912d190a6e01c765cb769d3b927766499
41324870 <41324870@087857-P33S105.macklabs.br>
2014-09-18 11:48:05 -0300

    corrigido

--- teste.php
+++ teste.php
@@ -3,7 +3,20 @@
     <head></head>
     <body>
         <?php include "operacoes.php"; 
-        
+        $x=10;
+        $y=5;
+        $resultado1 = soma($x, $y);
+        imprime_resultado($resultado1);
+        echo "<br>";
+        $resultado2=  subtracao($x, $y);
+        imprime_resultado($resultado2);
+        echo "<br>";
+        $resultado3= divisao($x, $y);
+        imprime_resultado($resultado3);
+        echo "<br>";
+        $resultado= multiplicacao($x, $y);
+        imprime_resultado($resultado);
+        echo "<br>";
    
         ?>
         
